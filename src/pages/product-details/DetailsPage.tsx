c8a53d4437b4f7c1047cdd05558bbe7899060151
Rafeeque386 <rafeeque386@gmail.com>
2024-05-26 18:03:42 +0530

    feat: :art: go back icon

--- src/pages/product-details/DetailsPage.tsx
+++ src/pages/product-details/DetailsPage.tsx
@@ -4,16 +4,22 @@ import './details.css';
 import { CarColorPicker } from '@/components/colorSelector/Selector';
 import { ICarImages, ICard } from '@/interfaces/model';
 import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { FaArrowCircleLeft } from 'react-icons/fa';
 
 const DetailsPage: React.FC = () => {
   const { id } = useParams();
   const parsedId = id ? parseInt(id, 10) : undefined;
   const [selectedColor, setSelectedColor] = useState<keyof ICarImages>('red');
+  const navigate = useNavigate();
 
   const car: ICard | undefined = teslaCars.find((c) => c.id === parsedId);
 
   const handleColorChange = (color: keyof ICarImages) => {
     setSelectedColor(color);
+  };
+  const goBack = () => {
+    navigate(-1); // Simulate going back by navigating with a negative delta
   };
 
   if (!car || !car.images) {
@@ -22,6 +28,11 @@ const DetailsPage: React.FC = () => {
 
   return (
     <div className="car-details-page">
+      <span onClick={goBack}>
+        {' '}
+        <FaArrowCircleLeft className="custom-icon" />
+      </span>
+
       <div className="car-details-container">
         <div className="car-image">
           <img src={car.images[selectedColor]} alt={`${car.name} in ${selectedColor.slice(1)}`} />
