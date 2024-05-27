2ef15083db9fe4ba09617ffa875c196e2d1c5cbe
Rafeeque386 <rafeeque386@gmail.com>
2024-05-27 11:12:01 +0530

    feat: :art: setting config file

---
+++ src/config/env.ts
@@ -0,0 +1,5 @@
+export const Env = {
+  api: {
+    API_URL: import.meta.env.VITE_TESLA_BACKEND_URL,
+  },
+};
--- src/services/carServices.ts
+++ src/services/carServices.ts
@@ -1,12 +1,13 @@
+import { Env } from '@/config/env';
 import { ICard } from '@/interfaces/model';
 
 export const fetchCars = async (): Promise<ICard[]> => {
-  const response = await fetch('http://localhost:8000/cars');
+  const response = await fetch(`${Env.api.API_URL}/cars`);
   return await response.json();
 };
 
 export const fetchCarById = async (id: number): Promise<ICard | undefined> => {
-  const response = await fetch(`http://localhost:8000/cars/${id}`);
+  const response = await fetch(`${Env.api.API_URL}/cars/${id}`);
   if (response.ok) {
     return await response.json();
   }
